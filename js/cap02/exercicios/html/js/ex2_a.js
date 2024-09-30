cf06988a11f96d87e73325ad267be3a4eb38b931
Lucast34 <lucast34t@gmail.com>
2024-09-30 20:27:04 -0300

    ex_a.html

--- js/cap02/exercicios/html/js/ex2_a.js
+++ js/cap02/exercicios/html/js/ex2_a.js
@@ -1,1 +1,13 @@
 const frm = document.querySelector("form");
+const resp1 = document.querySelector("out-R1");
+const resp2 = document.querySelector("out-R2");
+const resp3 = document.querySelector("out-R3");
+
+frm.addEventListener("submit", (e)=>{
+    const med = frm.inMedicamento.value
+    const preco = Number(frm.inPreco.value)
+
+    resp1.innerText= `O Preço é (o valor já descontado)${preco.toFixed(2)}`
+    
+    e.preventDefault()
+})
