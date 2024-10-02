8048d7c77958253f1f1c4edd9a214aeb52fa2a3b
Lucast34 <lucast34t@gmail.com>
2024-10-01 21:35:59 -0300

    ex04

--- js/cap04/js-ex/ex4_1.js
+++ js/cap04/js-ex/ex4_1.js
@@ -1,22 +1,29 @@
 const frm = document.querySelector('form');
+const resp1 = document.querySelector('h3');
+const resp2 = document.querySelector('h4');
+
 
 frm.addEventListener('submit', (e) => {
     e.preventDefault();
     const nome = frm.inNome.value;
     //alert(nome)
-    const nota1 = Number( frm.inN1.value);
-    const nota2 = Number( frm.inN2.value);
+    const nota1 = Number(frm.inN1.value);
+    const nota2 = Number(frm.inN2.value);
 
-    const media = (nota1 + nota2)/2;
-    
-    alert(media.toFixed(2));
+    const media = (nota1 + nota2) / 2;
 
-// se media >=7 então "Aprovado" senão "Reprovado"
-// alert(media >= 7)
-if (media >= 7){
-    alert("Aprovado ")
-} else{
-    alert("Reprovado")
-}
-    
+    //alert(media.toFixed(2));
+    resp1.innerText = `Média das notas:${media.toFixed(2)}`
+    // se media >=7 então "Aprovado" senão "Reprovado"
+    // alert(media >= 7)
+    if (media >= 7) {
+        //alert("Aprovado ")
+        resp2.innerText = `Prabéns ${nome}! Você foi aprovador!`;
+        resp2.style.color = 'blue';
+    } else {
+        //alert("Reprovado")
+        resp2.innerText = `Ops ${nome}! Você foi reprovado!`;
+        resp2.style.color = 'red';
+    }
+
 })
