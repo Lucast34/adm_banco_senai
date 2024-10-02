d5a027fb98bcbb9d49dd5fca082f2ac7460016e5
Lucast34 <lucast34t@gmail.com>
2024-10-01 21:08:47 -0300

    if,else

--- js/cap04/js-ex/ex4_1.js
+++ js/cap04/js-ex/ex4_1.js
@@ -7,8 +7,16 @@ frm.addEventListener('submit', (e) => {
     const nota1 = Number( frm.inN1.value);
     const nota2 = Number( frm.inN2.value);
 
-    const media = (nota1 + nota2)/2
+    const media = (nota1 + nota2)/2;
     
-    alert(media.toFixed(2))
+    alert(media.toFixed(2));
+
+// se media >=7 então "Aprovado" senão "Reprovado"
+
+if (media >= 7){
+    alert("Aprovado ")
+} else{
+    alert("Reprovado")
+}
     
 })
