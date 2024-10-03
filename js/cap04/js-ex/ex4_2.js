768633ffe2af04978eb4c61454ed84d343d40ed7
Lucast34 <lucast34t@gmail.com>
2024-10-02 21:46:26 -0300

    cap04

---
+++ js/cap04/js-ex/ex4_2.js
@@ -0,0 +1,32 @@
+function calcular (){
+    
+    const nome = document.getElementById('inNom').value; 
+    const masculimo = document.getElementById('inMas').checked; 
+    const altura = document.getElementById('inAL').value; 
+
+    let peso;
+    if(masculimo){
+        peso = 22 * Math.pow(altura,2)
+    }else{
+        peso = 21 * Math.pow(altura,2)
+    }
+
+    document.querySelector("h3").innerText = `Peso ideal ${peso.toFixed(2)}`;
+
+    return false; // impedir o refresh da página equivalente ao e.preventDefault()
+}
+
+
+
+
+
+
+
+// const frm = document.querySelector('form');
+// const resp = document.querySelector('h3');
+
+// frm.addEventListener('submit', (e) => {
+//     e.preventDefault()
+//     const nome = frm.inNome.value
+//     const masculino = frm.inMas
+// )}
