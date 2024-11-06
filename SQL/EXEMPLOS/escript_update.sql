b940a11e9ef487219565da77895f4becaa173716
L <lucast34t@gmail.com>
2024-11-05 21:46:29 -0300

    Add files via upload

---
+++ SQL/EXEMPLOS/escript_update.sql
@@ -0,0 +1,12 @@
+use campeonatobrasileiro;
+
+update time set id_estadio = 18 where id_time = 1;
+update time set id_estadio = 11 where id_time = 2;
+update time set id_estadio = 3 where id_time = 3;
+update time set id_estadio = 13 where id_time = 4;
+update time set id_estadio = 7 where id_time = 5;
+update time set id_estadio = 10 where id_time = 6;
+update time set id_estadio = 9 where id_time = 7;
+update time set id_estadio = 3 where id_time = 8;
+update time set id_estadio = 12 where id_time = 9 ;
+update time set id_estadio = 1 where id_time = 10;
