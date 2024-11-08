876f8cc5b3efdccd8cb83ee73d050a400f9eb5d9
L <lucast34t@gmail.com>
2024-11-08 20:08:22 -0300

    Delete SQL/EX/dbmaternidadeinsert.sql

--- SQL/EX/dbmaternidadeinsert.sql
+++
@@ -1,8 +0,0 @@
-use dbmaternidade;
-select * from medico;
-
-insert into medico(crm, nome, telefone, especialidade)
-values 
-('1010','Dr. João Silva','(11)92345-6789','Pediatra'),
-('1020','Dra. Maria Oliveira','(71)98765-4321','Pediatra'),
-('1030','Dr. Carlos Santos','(21)91234-5678','Pediatra');
