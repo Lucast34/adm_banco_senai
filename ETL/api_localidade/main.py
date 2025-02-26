85f6a26ea8bfb35cfd71a57062dd9e6a00f161f7
Lucast34 <lucast34t@gmail.com>
2025-02-25 21:40:57 -0300

    ETL2

--- ETL/api_localidade/main.py
+++ ETL/api_localidade/main.py
@@ -19,3 +19,11 @@ def banco(paramentro):
 url =  'https://servicodados.ibge.gov.br/api/v1/localidades/regioes'
 url2 = 'https://servicodados.ibge.gov.br/api/v1/localidades/estados'
 url3 = 'https://servicodados.ibge.gov.br/api/v1/localidades/municipios'
+
+response = requests.get(url)
+response_2 = requests.get(url2)
+response_3= requests.get(url3)
+
+obj = response.json()
+
+lista = obj['id']['sigla']['nome']
