f5e31449a2be199a09830d260cb16c9e146885e3
Lucast34 <lucast34t@gmail.com>
2025-02-26 21:06:20 -0300

    Ibge

--- ETL/api_localidade/main.py
+++ ETL/api_localidade/main.py
@@ -3,10 +3,10 @@ import mysql.connector
 
 
 
-def banco(paramentro):
+def banco(sql):
     conexao = mysql.connector.connect(
         host ='localhost',
-        database = 'pizzaria',
+        database = 'test_pythonIbge',
         user = 'root',
         password = 'senai@123'
     )
@@ -17,13 +17,31 @@ def banco(paramentro):
     conexao.close()
 
 url =  'https://servicodados.ibge.gov.br/api/v1/localidades/regioes'
-url2 = 'https://servicodados.ibge.gov.br/api/v1/localidades/estados'
+
+url2 = 'https://servicodados.ibge.gov.br/api/v1/localidades/estados' 
+
 url3 = 'https://servicodados.ibge.gov.br/api/v1/localidades/municipios'
 
 response = requests.get(url)
+
 response_2 = requests.get(url2)
+
 response_3= requests.get(url3)
 
 obj = response.json()
 
-lista = obj['id']['sigla']['nome']
+# Como os json já em um array com isso não há  uma necessidade "filtrar"
+
+lista = obj
+
+i = 0
+
+while i < len(lista):
+    id = lista[i]['id']
+    sigla = lista[i]['sigla']
+    nome = lista[i]['nome']
+
+    sql = f"INSERT INTO macroregiao(id_regiao, sigla, nome) VALUES ('{id}', '{sigla}', '{nome}');"
+
+    banco(sql)
+    i += 1
